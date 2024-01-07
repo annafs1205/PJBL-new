882b1b0cf76c81d5357e5aef74c4eaf7c13ec752
annafs1205 <ladhwan.naah@gmail.com>
2024-01-07 12:27:32 +0700

    PJBL

---
+++ Assets/Asset Game/scene_loader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class scene_loader : MonoBehaviour
+{
+    public Image loadingfill;
+
+    void Start() {
+        StartCoroutine(loading());
+    }
+
+   IEnumerator loading() {
+        AsyncOperation loading = SceneManager.LoadSceneAsync("Buat Akun"); 
+
+        while (!loading.isDone){
+            loadingfill.fillAmount = loading.progress/0.9f;
+            yield return null;
+        }
+    }
+}
